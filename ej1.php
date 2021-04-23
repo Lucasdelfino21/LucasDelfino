3a2a634268843d6fb2aa7f8140086ff9451951c2
Lucas Delfino <lucasdelfino2121@gmail.com>
2021-04-23 17:40:17 -0300

    Commit

---
+++ ej1.php
@@ -0,0 +1,6 @@
+<?php
+    $n1 = 2;
+    $n2 = 4;
+    $suma = $n1 + $n2;
+    echo"El resultado es: ", $suma;
+?>
---
+++ ej4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form name="formularioDatos" method="post" action="ej4-2.php">
+        <p> Calculo el mayor </p>
+        <br/>
+        Introduzca el primer numero: <input type="text" name="num1" value="">
+        <br/> <br/>
+        Introduzca el segundo numero: <input type="text" name="num2" value="">
+        <br/> <br/>
+        <input value="Calcular mayor" type="submit" />
+    </form>
+</body>
+</html>
